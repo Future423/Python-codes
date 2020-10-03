ee3d7d986333dbb5bcd1e794e6d7e82273e24fae
Hacker <72150854+Futurehacker812@users.noreply.github.com>
2020-10-03 09:54:47 +0530

    Update wiki.py

--- wiki.py
+++ wiki.py
@@ -1,14 +1,20 @@
+#importing modules
 import wikipedia
+#first you have to install wikipedia module as it is not in-built
 import warnings
+#warning is in-built module
+#this module in not important for this program
+#this module hide warning from module, if occurs
 
+# initializing a function
 def mywiki(a):
-    import wikipedia
-    import warnings
-
+    
+    #try and except hideing errors, if occurs
     try:
         warnings.filterwarnings("ignore")
         result = wikipedia.summary(a, sentences = 3)
         return(result)
+    
     except Exception as e:
         print(a,"is unable to find easily.There are many ",a,"which one do you want to find...")
         return(e)
