8aa79a5e5a4dafae6ba8673a186828ba734881ab
Hacker <72150854+Futurehacker812@users.noreply.github.com>
2020-10-23 21:07:26 +0530

    Update and rename resize.py to Resize-GUI.py

--- Resize-GUI.py
+++ Resize-GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 root = Tk()
 root.geometry("220x150")
 root.title('Nishant')
@@ -8,9 +9,11 @@ def update():
 
 width=StringVar()
 height=StringVar()
+
 Label(root, text="Width",padx=8,pady=8,font='comicsansms 16').grid()
 Label(root, text="Height",font='comicsansms 16 ').grid(row=1)
 Entry(root, textvariable=width).grid(row=0,column=1)
 Entry(root, textvariable=height).grid(row=1,column=1)
 Button(root, text='Apply',font='comicsansms 12',command=update).grid(row=2,column=1)
+
 root.mainloop()
