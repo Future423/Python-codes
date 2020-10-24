e3a5b8d5c1bdd8600801b7ef8cfd39a5ab222b36
Hacker <72150854+Future423@users.noreply.github.com>
2020-10-24 21:09:58 +0530

    Update and rename opening.py to OpeningOtherPrograms.py

---
+++ OpeningOtherPrograms.py
@@ -0,0 +1,14 @@
+#This is python code for opening other programs\applications using python
+#First you had to import "os"nmodule
+import os
+#This module is in-built in python 3
+
+#Now just write "os.startfile("proper address of program as string")"
+
+#os.startfile("C://Windows//system32//mspaint.exe")
+os.startfile(r"C:\Windows\system32\mspaint.exe")
+#These are two ways to give address
+
+#And when you run/compile it "paint will open"
+
+#You can open any program/application using it
