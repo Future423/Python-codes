e524361c35bc97a55779aa8d63438a4a66eb1621
Hacker <72150854+Future423@users.noreply.github.com>
2020-12-17 22:40:37 +0530

    Update spek.py

--- spek.py
+++ spek.py
@@ -1,13 +1,22 @@
 import pyttsx3
+#this module is not in-built in your python
+#you had to install it using pip command
+
 engine = pyttsx3.init('sapi5')
+#sapi5, windows voice
+
 rate = engine.getProperty('rate')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[5].id)
-engine.setProperty('rate', 125)
+#may be your Pc have many voices choose one out of them in "voices[<>]"
 
+engine.setProperty('rate', 125)
+#rate is speed of speaking, you can change it
+
+#inetialing a function
 def spk(audio):
     engine.say(audio)
     engine.runAndWait()
 
-spk('''I'm fine
-very fine''')
+spk('I can speak')
+#write in spk(<here>),what you had to make your code speak
