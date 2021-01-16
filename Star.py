7c93a444a8e3879386ec71a2412badc3f8be98fb
Hacker <72150854+Future423@users.noreply.github.com>
2021-01-16 21:34:18 +0530

    Update and rename caption star.py to Star.py

--- Star.py
+++ Star.py
@@ -1,8 +1,17 @@
 import turtle as t
+
 t.bgcolor("black")
+#setting background color <black>
+
 t.color("red")
+#setting pen color <red>
+
 t.hideturtle()
-t.speed(100)
+#hiding turtle
+
+t.speed(0)
+#setting speed <max speed>
+
 for i in range(500):
     t.forward(i)
     t.right(144)
