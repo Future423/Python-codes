f0503b23f4a52f0e6c6234b9a2604bca0cc67429
Hacker <72150854+Future423@users.noreply.github.com>
2021-01-17 19:26:41 +0530

    Add files via upload

---
+++ Windmill.py
@@ -0,0 +1,13 @@
+import turtle as t
+t.speed(100)
+t.bgcolor("black")
+t.pencolor("red")
+for i in range(190):
+    t.left(30)
+    t.forward(i)
+    t.right(120)
+    t.forward(i)
+    t.right(120)
+    t.forward(i)
+    t.right(90)
+    t.forward(i)
