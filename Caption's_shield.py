9681bac8829ecf0038837921358d0521b804347b
Hacker <72150854+Future423@users.noreply.github.com>
2021-04-20 08:12:15 +0530

    Update and rename cap.py to Caption's_shield.py

--- Caption's_shield.py
+++ Caption's_shield.py
@@ -3,27 +3,33 @@ t.bgcolor("white")
 t.pencolor("red")
 t.hideturtle()
 t.speed(0)
+
 t.penup()
 t.goto(0,-200)
 t.pendown()
 t.pensize(5)
 t.circle(240)
+
 t.penup()
 t.goto(0,-160)
 t.pendown()
 t.circle(200)
+
 t.penup()
 t.goto(0,-120)
 t.pendown()
 t.circle(160)
+
 t.penup()
 t.goto(0,-80)
 t.pendown()
 t.circle(120)
+
 t.penup()
 t.goto(0,40)
 t.pendown()
 t.pencolor("blue")
-for i in range(220):
+
+for i in range(220):    #for star
     t.forward(i)
     t.right(144)
