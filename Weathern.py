71941e5e1ef800ea6238c93bf056311158c03c9e
Hacker <72150854+Future423@users.noreply.github.com>
2021-05-06 14:15:35 +0530

    Add files via upload

---
+++ Weathern.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+import requests
+import time
+ 
+
+def getWeather(canvas):
+    city = textField.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06c921750b9a82d8f5d1294e1586276f"
+    
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    min_temp = int(json_data['main']['temp_min'] - 273.15)
+    max_temp = int(json_data['main']['temp_max'] - 273.15)
+    pressure = json_data['main']['pressure']
+    humidity = json_data['main']['humidity']
+    wind = json_data['wind']['speed']
+    sunrise = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunrise'] - 21600))
+    sunset = time.strftime('%I:%M:%S', time.gmtime(json_data['sys']['sunset'] - 21600))
+
+    final_info = condition + "\n" + str(temp) + "°C" 
+    final_data = "\n"+ "Min Temp: " + str(min_temp) + "°C" + "\n" + "Max Temp: " + str(max_temp) + "°C" +"\n" + "Pressure: " + str(pressure) + "\n" +"Humidity: " + str(humidity) + "\n" +"Wind Speed: " + str(wind) + "\n" + "Sunrise: " + sunrise + "\n" + "Sunset: " + sunset
+    label1.config(text = final_info)
+    label2.config(text = final_data)
+
+canvas = tk.Tk()
+canvas.geometry("500x550")
+canvas.title("Weather Window")
+canvas.configure(background="light cyan")
+f = ("Bradley Hand ITC",20, "bold")
+t = ("Baskerville Old Face", 35)
+
+textField = tk.Entry(canvas, justify='center', width = 20, font = t)
+textField.pack(pady = 20)
+textField.focus()
+textField.bind('<Return>', getWeather)
+
+label1 = tk.Label(canvas, font=t,bg='light cyan')
+label1.pack()
+label2 = tk.Label(canvas,bg='light cyan', font=f)
+label2.pack()
+canvas.mainloop()
