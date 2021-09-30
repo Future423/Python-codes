5ef097ca34c34a9fe12e392cffb0e4af3401c9d2
Parul <87116013+Cyber-Cat15@users.noreply.github.com>
2021-09-30 22:32:48 +0530

    removed error

--- EvenOdd.py
+++ EvenOdd.py
@@ -1,5 +1,5 @@
 a = input ("Enter any number:")
-if a%2==0
-  print ("Entered number is even")
-else
-  print ("Entered number is odd")
+if a%2==0:
+  print ("Entered number is even:")
+else:
+  print ("Entered number is odd:")
