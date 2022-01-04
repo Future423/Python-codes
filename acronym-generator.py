99a8cf4cf59f9cbd878a5c4da927e93c20c751f6
Nishant <72150854+Future423@users.noreply.github.com>
2022-01-04 20:54:07 +0530

    Delete acronym-generator.py

--- acronym-generator.py
+++
@@ -1,8 +0,0 @@
-user_input = input("Enter a phrase: ")
-phrase = (user_input.replace('of', '')).split()
-acronym = ""
-
-for word in phrase:
-    acronym = acronym + word[0].upper()
-    
-print(f'Acronym of {user_input} is {acronym}')
